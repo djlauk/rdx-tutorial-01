350ef2b7dd598fbbb4a2ffac13661ec66cf4825c
Daniel J. Lauk <daniel.lauk@gmail.com>
2020-05-21 14:46:02 +0200

    move state management to rdx

--- app.js
+++ app.js
@@ -1,33 +1,55 @@
-// our applications state: the message to display
-let message = "";
-let numClicks = 0;
+import {Store} from '@captaincodeman/rdx';
+
+// our application's state
+const initialState = {
+    message: '',
+    numClicks: 0,
+}
+
+// the reducer: how to get from one state to the next
+const greetingReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'hello':
+            return {...state, message: 'Hello, rdx!', numClicks: state.numClicks + 1}
+        case 'howdy':
+            return {...state, message: 'Howdy, rdx!', numClicks: state.numClicks + 1}
+        case 'hi':
+            return {...state, message: 'Hi, rdx!', numClicks: state.numClicks + 1}
+        default:
+            return state;
+    }
+};
+
+// the store is the engine for managing the state
+const store = new Store(initialState, greetingReducer);
+
+// when there was an update, the store will tell us through events
+store.addEventListener('state', () => {
+    updateGreeting();
+    updateCounter();
+});
 
 function updateGreeting() {
     const el = document.getElementById('greeting');
-    el.innerText = message;
+    el.innerText = store.state.message;
 }
 
 function updateCounter() {
     const el = document.getElementById('counter');
-    el.innerText = `Processed ${numClicks} ${numClicks === 1 ? 'click' : 'clicks'}.`;
+    const n = store.state.numClicks;
+    el.innerText = `Processed ${n} ${n === 1 ? 'click' : 'clicks'}.`;
 }
 
-// make the buttons change the message
+// make the buttons inform the store
 document.getElementById('btnHello').addEventListener('click', () => {
-    message = "Hello, rdx!";
-    updateGreeting();
-    numClicks++;
-    updateCounter();
+    const action = { type: 'hello' };
+    store.dispatch(action);
 });
 document.getElementById('btnHowdy').addEventListener('click', () => {
-    message = "Howdy, rdx!";
-    updateGreeting();
-    numClicks++;
-    updateCounter();
+    const action = { type: 'howdy' };
+    store.dispatch(action);
 });
 document.getElementById('btnHi').addEventListener('click', () => {
-    message = "Hi, rdx!";
-    updateGreeting();
-    numClicks++;
-    updateCounter();
+    const action = { type: 'hi' };
+    store.dispatch(action);
 });
